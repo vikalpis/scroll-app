30544f304e627e88e33d757998d1e1c0dedc46d4
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-02-17 21:38:54 +0530

    Add NextAuth integration with credentials provider for user authentication

---
+++ lib/auth.ts
@@ -0,0 +1,80 @@
+import { NextAuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import Email from "next-auth/providers/email";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+import { connectionToDatabase } from "./db";
+export const authOptions : NextAuthOptions ={
+    providers : [
+        CredentialsProvider(
+           {
+            name : "Credentials",
+            credentials : {
+                email : {label : "Email" , type : "text"},
+                password : {label : "Password" , type : "password"}
+            },
+            async authorize(credentials){
+                if(!credentials?.email || !credentials?.password){
+                    throw new Error ("Missing email or password");
+                }
+
+                try {
+                    await connectionToDatabase()
+                   const user = await User.findOne({
+                        email : credentials.email,
+                        
+
+                    })
+                    if(!user){
+                        throw new Error("No user found");
+                    }
+
+                  const isVlid =  await bcrypt.compare(
+                        credentials.password , 
+                       user.password
+                    );
+
+                    if(!isVlid){
+                        throw new Error("Invalid password");
+                    }
+
+                    return {
+                        id :user._id.toString(),
+                        email : user.email,
+                    }
+
+                } catch (error) {
+                    throw error;
+                }
+            }
+           }
+        )
+    ],
+
+    callbacks : {
+        async jwt ( {token , user}) {
+            if(user){
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session ({session, token}){
+            
+            if(session.user){
+                session.user.id = token.id as string ;
+            }
+
+            return session
+        }
+    },  
+
+    pages : {
+        signIn : "/login",
+        error : "/login"
+    },
+    session :{
+        strategy : "jwt",
+        maxAge : 30 * 24 * 60 * 60,
+    },
+    secret : process.env.NEXTAUTH_SECRET
+}
