04f8494a011bcbd65ffbf47159723510b541e285
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-03-16 21:50:16 +0530

    Add VideoUploadForm component and integrate react-hook-form for video uploads

---
+++ app/component/VideoUploadForm.tsx
@@ -0,0 +1,7 @@
+"use client "
+import { useState } from "react"
+import {useForm} from "react-hook-form";
+import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
+import { Loader2 } from "lucide-react";
+import { useNotification } from "./Notification";
+
