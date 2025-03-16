92077301ee66e51d22771d84cf927de6e1f3dbcf
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-03-16 21:20:52 +0530

    Refactor video model and component structure; consolidate video schema and implement new VideoComponent

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectionToDatabase } from "@/lib/db";
-import Video, {IVideo} from "@/models/Videos";
+import Video, {IVideo} from "@/models/Video";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
